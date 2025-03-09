681f95dee32527adcaf5e7df109c1e6e1623d373
SlyFoxy <vadimlisovoi@gmail.com>
2025-03-09 18:54:09 +0200

    roflanpush5

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 
 //----------------------------LAB 1-----------------------------------------------
-//----------------------------LAB 1-----------------------------------------------
-//----------------------------LAB 1-----------------------------------------------
+// Фабричний метод та Абстрактна фабрика
 abstract class Dish
 {
     public abstract string Prepare();
@@ -11,18 +10,12 @@ abstract class Dish
 
 class Pizza : Dish
 {
-    public override string Prepare()
-    {
-        return "Готується піца з інгредієнтами.";
-    }
+    public override string Prepare() => "Готується піца з інгредієнтами.";
 }
 
 class Pasta : Dish
 {
-    public override string Prepare()
-    {
-        return "Готується паста з соусом.";
-    }
+    public override string Prepare() => "Готується паста з соусом.";
 }
 
 abstract class DishFactory
@@ -32,49 +25,19 @@ abstract class DishFactory
 
 class PizzaFactory : DishFactory
 {
-    public override Dish CreateDish()
-    {
-        return new Pizza();
-    }
+    public override Dish CreateDish() => new Pizza();
 }
 
 class PastaFactory : DishFactory
 {
-    public override Dish CreateDish()
-    {
-        return new Pasta();
-    }
+    public override Dish CreateDish() => new Pasta();
 }
 
-//------------------------------------LAB 2-------------------------------------------
-//------------------------------------LAB 2-------------------------------------------
-//------------------------------------LAB 2-------------------------------------------
+//----------------------------LAB 2-----------------------------------------------
 // Прототип
 public interface ICloneableOrder
 {
     Order Clone();
-}
-
-public class Order : ICloneableOrder
-{
-    public List<string> Dishes { get; private set; } = new List<string>();
-
-    public void AddDish(string dish)
-    {
-        Dishes.Add(dish);
-    }
-
-    public Order Clone()
-    {
-        Order copy = new Order();
-        copy.Dishes = new List<string>(this.Dishes);
-        return copy;
-    }
-
-    public void ShowOrder()
-    {
-        Console.WriteLine("Order contains: " + string.Join(", ", Dishes));
-    }
 }
 
 // Будівельник
@@ -87,16 +50,10 @@ public interface IOrderBuilder
 public class OrderBuilder : IOrderBuilder
 {
     private Order _order = new Order();
-    
-    public void AddDish(string dish)
-    {
-        _order.AddDish(dish);
-    }
 
-    public Order Build()
-    {
-        return _order;
-    }
+    public void AddDish(string dish) => _order.AddDish(dish);
+
+    public Order Build() => _order;
 }
 
 public class Director
@@ -110,44 +67,172 @@ public class Director
     }
 }
 
+//----------------------------LAB 3-----------------------------------------------
+// Стратегія
+public interface IPricingStrategy
+{
+    decimal CalculatePrice(Order order);
+}
+
+public class RegularPricing : IPricingStrategy
+{
+    public decimal CalculatePrice(Order order) => order.BasePrice;
+}
+
+public class DiscountPricing : IPricingStrategy
+{
+    public decimal CalculatePrice(Order order) => order.BasePrice * 0.9m;
+}
+
+public class PremiumPricing : IPricingStrategy
+{
+    public decimal CalculatePrice(Order order) => order.BasePrice * 1.2m;
+}
+
+// Спостерігач
+public interface IOrderObserver
+{
+    void Update(Order order);
+}
+
+public class Waiter : IOrderObserver
+{
+    public void Update(Order order) => Console.WriteLine("Офіціант отримав оновлення: Нове замовлення!");
+}
+
+public class Chef : IOrderObserver
+{
+    public void Update(Order order) => Console.WriteLine("Шеф-кухар отримав оновлення: Нове замовлення на кухні!");
+}
+
+public class OrderNotifier
+{
+    private List<IOrderObserver> _observers = new List<IOrderObserver>();
+
+    public void Attach(IOrderObserver observer) => _observers.Add(observer);
+    public void Detach(IOrderObserver observer) => _observers.Remove(observer);
+    public void Notify(Order order)
+    {
+        foreach (var observer in _observers)
+        {
+            observer.Update(order);
+        }
+    }
+}
+
+// Команда
+public interface ICommand
+{
+    void Execute();
+    void Undo();
+}
+
+public class AddDishCommand : ICommand
+{
+    private Order _order;
+    private string _dish;
+
+    public AddDishCommand(Order order, string dish)
+    {
+        _order = order;
+        _dish = dish;
+    }
+
+    public void Execute()
+    {
+        _order.AddDish(_dish);
+        Console.WriteLine($"Страва {_dish} додана до замовлення.");
+    }
+
+    public void Undo()
+    {
+        _order.RemoveDish(_dish);
+        Console.WriteLine($"Страва {_dish} видалена з замовлення.");
+    }
+}
+
+public class Order : ICloneableOrder
+{
+    public decimal BasePrice { get; private set; } = 100;
+    public List<string> Dishes { get; private set; } = new List<string>();
+    public IPricingStrategy PricingStrategy { get; set; }
+    public OrderNotifier Notifier { get; private set; } = new OrderNotifier();
+
+    public void AddDish(string dish)
+    {
+        Dishes.Add(dish);
+        Notifier.Notify(this);
+    }
+
+    public void RemoveDish(string dish)
+    {
+        Dishes.Remove(dish);
+        Notifier.Notify(this);
+    }
+
+    public decimal GetFinalPrice() => PricingStrategy?.CalculatePrice(this) ?? BasePrice;
+
+    public Order Clone()
+    {
+        Order copy = new Order { PricingStrategy = this.PricingStrategy };
+        copy.Dishes.AddRange(this.Dishes);
+        return copy;
+    }
+
+    public void ShowOrder()
+    {
+        Console.WriteLine("Order contains: " + string.Join(", ", Dishes));
+    }
+}
+
 
 class Program
 {
     static void Main()
     {
         //------------------------LAB 1-----------------------------------
-        //------------------------LAB 1-----------------------------------
-        //------------------------LAB 1-----------------------------------
         DishFactory pizzaFactory = new PizzaFactory();
         DishFactory pastaFactory = new PastaFactory();
         
-        Console.WriteLine(OrderDish(pizzaFactory));  // Готується піца з інгредієнтами.
-        Console.WriteLine(OrderDish(pastaFactory));  // Готується паста з соусом.
+        Console.WriteLine(OrderDish(pizzaFactory));
+        Console.WriteLine(OrderDish(pastaFactory));
 
-        //----------------------LAB 2------------------------------------------
-        //----------------------LAB 2------------------------------------------
-        //----------------------LAB 2------------------------------------------
+        //----------------------LAB 2-------------------------------------
         Console.WriteLine("--- Prototype Pattern ---");
         Order originalOrder = new Order();
         originalOrder.AddDish("Pizza");
         originalOrder.AddDish("Pasta");
-        
+
         Order clonedOrder = originalOrder.Clone();
         clonedOrder.ShowOrder();
-        
+
         Console.WriteLine("--- Builder Pattern ---");
         IOrderBuilder builder = new OrderBuilder();
         builder.AddDish("Steak");
         builder.AddDish("Salad");
-        
+
         Order customOrder = builder.Build();
         customOrder.ShowOrder();
-        
+
         Director director = new Director();
         Order comboMeal = director.CreateComboMeal(new OrderBuilder());
         comboMeal.ShowOrder();
+
+        //----------------------LAB 3-------------------------------------
+        Order order = new Order();
+        order.PricingStrategy = new DiscountPricing();
+
+        Waiter waiter = new Waiter();
+        Chef chef = new Chef();
+        order.Notifier.Attach(waiter);
+        order.Notifier.Attach(chef);
+
+        ICommand addDish = new AddDishCommand(order, "Піца");
+        addDish.Execute();
+        Console.WriteLine($"Фінальна ціна: {order.GetFinalPrice()} грн");
+        addDish.Undo();
     }
-    
+
     //--------------------LAB 1------------------------------
     static string OrderDish(DishFactory factory)
     {
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab9c69b4c567ac980afe7e20c4fc809e70117e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c641d8889d0e9e9e16d341fec0be24054f02b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
